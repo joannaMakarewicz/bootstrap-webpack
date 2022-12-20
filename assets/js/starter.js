d2d8ed9ab1789ff0a976db80d84ef49f3f9d19f2
Joanna Makarewicz <joanna.milun@gmail.com>
2022-12-20 10:02:32 +0100

    Empty project for new structure on new branch

--- assets/js/starter.js
+++
@@ -1,6 +0,0 @@
-
-import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
